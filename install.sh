cff4e4b9c997b6b8cbab7c63c4898403eceb3136
Tomas Čerkauskas <tomas@cerkauskas.lt>
2018-08-25 09:35:52 +0300

    add: .bashrc installation

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MAIN_DIR="`dirname \"$0\"`"            # relative
+MAIN_DIR="`( cd \"$MAIN_DIR\" && pwd )`"  # absolutized and normalized
+if [ -z "$MAIN_DIR" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+ln -s "$MAIN_DIR/.bashrc" ~/.bashrc
