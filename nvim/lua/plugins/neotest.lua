3b6c1913fbeb7c4ce50c2493c6c49e8174a8ad37
Tomas Čerkauskas <tomas@chronosphere.io>
2025-05-28 10:57:34 +0300

    improve performance of neotest on large codebases

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,115 +1,133 @@
 return {
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			{ "fredrikaverpil/neotest-golang", version = "*" }, -- Installation
-		},
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "fredrikaverpil/neotest-golang", version = "*" }, -- Installation
+    },
 
-		config = function()
-			local neotest_golang_opts = { go_test_args = { "-v" } } -- Specify custom configuration
-			require("neotest").setup({
-				adapters = {
-					require("neotest-golang")(neotest_golang_opts), -- Registration
-				},
-				output = {
-					enabled = true,
-					open_on_run = true,
-				},
-			})
+    config = function()
+      local neotest_golang_opts = { go_test_args = { "-v" } } -- Specify custom configuration
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang")(neotest_golang_opts), -- Registration
+        },
+        output = {
+          enabled = true,
+          open_on_run = true,
+        },
+        -- See all config options with :h neotest.Config
+        discovery = {
+          -- Drastically improve performance in ginormous projects by
+          -- only AST-parsing the currently opened buffer.
+          enabled = false,
+          -- Number of workers to parse files concurrently.
+          -- A value of 0 automatically assigns number based on CPU.
+          -- Set to 1 if experiencing lag.
+          concurrent = 1,
+        },
+        running = {
+          -- Run tests concurrently when an adapter provides multiple commands to run.
+          concurrent = true,
+        },
+        summary = {
+          -- Enable/disable animation of icons.
+          animated = false,
+        },
+      })
 
-			--vim.keymap.set("n", "<leader>tn", function()
-			--	require("neotest").run.run()
-			--end, {})
-		end,
-		keys = {
-			{
-				"<leader>ta",
-				function()
-					require("neotest").run.attach()
-				end,
-				desc = "[t]est [a]ttach",
-			},
-			{
-				"<leader>tf",
-				function()
-					require("neotest").run.run(vim.fn.expand("%"))
-				end,
-				desc = "[t]est run [f]ile",
-			},
-			{
-				"<leader>tA",
-				function()
-					require("neotest").run.run(vim.uv.cwd())
-				end,
-				desc = "[t]est [A]ll files",
-			},
-			{
-				"<leader>tS",
-				function()
-					require("neotest").run.run({ suite = true })
-				end,
-				desc = "[t]est [S]uite",
-			},
-			{
-				"<leader>tn",
-				function()
-					require("neotest").run.run()
-				end,
-				desc = "[t]est [n]earest",
-			},
-			{
-				"<leader>tl",
-				function()
-					require("neotest").run.run_last()
-				end,
-				desc = "[t]est [l]ast",
-			},
-			{
-				"<leader>ts",
-				function()
-					require("neotest").summary.toggle()
-				end,
-				desc = "[t]est [s]ummary",
-			},
-			{
-				"<leader>to",
-				function()
-					require("neotest").output.open({ enter = true, auto_close = true })
-				end,
-				desc = "[t]est [o]utput",
-			},
-			{
-				"<leader>tO",
-				function()
-					require("neotest").output_panel.toggle()
-				end,
-				desc = "[t]est [O]utput panel",
-			},
-			{
-				"<leader>tt",
-				function()
-					require("neotest").run.stop()
-				end,
-				desc = "[t]est [t]erminate",
-			},
-			{
-				"<leader>td",
-				function()
-					require("neotest").run.run({ suite = false, strategy = "dap" })
-				end,
-				desc = "Debug nearest test",
-			},
-			{
-				"<leader>tD",
-				function()
-					require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" })
-				end,
-				desc = "Debug current file",
-			},
-		},
-	},
+      --vim.keymap.set("n", "<leader>tn", function()
+      --	require("neotest").run.run()
+      --end, {})
+    end,
+    keys = {
+      {
+        "<leader>ta",
+        function()
+          require("neotest").run.attach()
+        end,
+        desc = "[t]est [a]ttach",
+      },
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "[t]est run [f]ile",
+      },
+      {
+        "<leader>tA",
+        function()
+          require("neotest").run.run(vim.uv.cwd())
+        end,
+        desc = "[t]est [A]ll files",
+      },
+      {
+        "<leader>tS",
+        function()
+          require("neotest").run.run({ suite = true })
+        end,
+        desc = "[t]est [S]uite",
+      },
+      {
+        "<leader>tn",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "[t]est [n]earest",
+      },
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "[t]est [l]ast",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "[t]est [s]ummary",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output.open({ enter = true, auto_close = true })
+        end,
+        desc = "[t]est [o]utput",
+      },
+      {
+        "<leader>tO",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "[t]est [O]utput panel",
+      },
+      {
+        "<leader>tt",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "[t]est [t]erminate",
+      },
+      {
+        "<leader>td",
+        function()
+          require("neotest").run.run({ suite = false, strategy = "dap" })
+        end,
+        desc = "Debug nearest test",
+      },
+      {
+        "<leader>tD",
+        function()
+          require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" })
+        end,
+        desc = "Debug current file",
+      },
+    },
+  },
 }
